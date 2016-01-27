63a3a06f7d51c68afeaa500815aac2f4b580ac36
Deepak Sharma <dpkshrma01@gmail.com>
2016-01-27 08:10:12 +0530

    AutoLaunch packaged app on startup

--- src/app/nw_win.js
+++ src/app/nw_win.js
@@ -1,12 +1,26 @@
 var gulp = require('gulp');
 var gui = require('nw.gui');
+var AutoLaunch = require('auto-launch');
 
+/* Auto launch packaged app on startup */
+var appLauncher = new AutoLaunch({
+    name: 'GoldFish'
+});
+
+appLauncher.isEnabled(function(enabled){
+    if(enabled) return;
+    appLauncher.enable(function(err){
+        // log error somewhere?
+    });
+});
+
+/* Get window instance, and show */
 var win = gui.Window.get();
+win.show();
 
+/* Livereload app during development */
 gulp.task('reload', function () {
     if (location) location.reload();
 });
 
 gulp.watch('**/*', ['reload']);
-
-win.show();
