c63cf25674f3a3ad2cc70a8652b7cf86fb7e5c1c
Deepak Sharma <dpkshrma01@gmail.com>
2016-02-07 01:35:17 +0530

    Add Collection Model

---
+++ src/app/models/collection.js
@@ -0,0 +1,19 @@
+var Model = require('nosql-schema-validator');
+
+var schema = {
+    _id       : Number,
+    name      : String,
+    keywords : [{
+        type: String
+    }],
+    bg_img    : String,
+    created_at: Date
+};
+
+var collection = function(data){
+    Model.call(this, schema, data);
+}
+
+collection.prototype = Object.create(Model.prototype);
+
+module.exports = collection;
